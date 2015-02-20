be40b9b44d39acb05afed148c7fddec8e53f8a8e
Moghedrin <nolat301@gmail.com>
2015-02-19 18:16:22 -0700

    Clean up regex based on @tianon's suggestions.

--- update.sh
+++ update.sh
@@ -2,9 +2,9 @@
 set -e
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
-
+pattern='.*?\/docs\/([0-9]+\.[0-9]+\.[0-9]+)\/learn.*'
 version=$(curl -sSL 'http://logstash.net' \
-	| sed -rn '0,/.*?\/docs\/.\..\..\/learn.*/s/.*?\/docs\/([0-9]*\.[0-9]*\.[0-9]*)\/learn.*/\1/gp')
+	| sed -rn "0,/${pattern}/s/${pattern}/\1/gp")
 
 sed -ri -e 's/^(ENV LOGSTASH_VERSION) .*/\1 '"$version"'/' "Dockerfile"
 
