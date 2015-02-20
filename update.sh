b4da2dfcbbe0bd6f8ae7556ad8e5c4be07d67469
Moghedrin <nolat301@gmail.com>
2015-02-19 17:45:09 -0700

    Fix pattern match.

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ set -e
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 version=$(curl -sSL 'http://logstash.net' \
-	| sed -rn '0,/.*?\/docs\/.\..\..\/learn.*/s/.*?\/docs\/(.\..\..)\/learn.*/\1/gp')
+	| sed -rn '0,/.*?\/docs\/.\..\..\/learn.*/s/.*?\/docs\/([0-9]*\.[0-9]*\.[0-9]*)\/learn.*/\1/gp')
 
 sed -ri -e 's/^(ENV LOGSTASH_VERSION) .*/\1 '"$version"'/' "Dockerfile"
 
