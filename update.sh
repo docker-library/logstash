b3dd51ac95012215d3537bbfc879624a65b0ee03
Joe Ferguson <joe@infosiftr.com>
2015-03-09 10:22:10 -0700

    Switch to apt repo

--- update.sh
+++ update.sh
@@ -2,12 +2,11 @@
 set -e
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
-pattern='.*?\/docs\/([0-9]+\.[0-9]+\.[0-9]+)\/learn.*'
-version=$(curl -sSL 'http://logstash.net' \
-	| sed -rn "0,/${pattern}/s/${pattern}/\1/gp")
-shaprime=$(curl -sSL "https://download.elasticsearch.org/logstash/logstash/logstash-${version}.tar.gz.sha1.txt" | sed -r -e 's/^(.+?)\s\s.*/\1/')
+
+version='1.4'
+fullVersion="$(curl -sSL "http://packages.elasticsearch.org/logstash/$version/debian/dists/stable/main/binary-amd64/Packages.gz" | gunzip | awk -F ': *' '$1 == "Package" { pkg = $2 } pkg == "logstash" && $1 == "Version" { print $2 }' | sort -V | tail -1)"
 
 sed -ri \
-	-e 's/^(ENV LOGSTASH_VERSION) .*/\1 '"$version"'/' \
-	-e 's/^(ENV LOGSTASH_DOWNLOAD_SHA1) .*/\1 '"$shaprime"'/' "Dockerfile"
+	-e 's/^(ENV LOGSTASH_MAJOR) .*/\1 '"$version"'/' \
+	-e 's/^(ENV LOGSTASH_VERSION) .*/\1 '"$fullVersion"'/' Dockerfile
 
