b35589d3f7d20e8b65bb87759985ae037a1a00e9
Tianon Gravi <admwiggin@gmail.com>
2015-05-14 16:04:13 -0600

    Update generate-stackbrew-library.sh for 1.5 (#15), now that upstream has an epoch and no useful extra information in their "debian version"

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -5,12 +5,13 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 url='git://github.com/docker-library/logstash'
 commit=$(git log -1 --format="format:%H" -- Dockerfile)
 debVersion="$(grep -m1 'ENV LOGSTASH_VERSION' Dockerfile | cut -d' ' -f3)"
-fullVersion="${debVersion%%[-]*}"
+fullVersion="${debVersion%%[-]*}" # strip "debian version"
+fullVersion="${fullVersion##*:}" # strip epoch
 version="${fullVersion%[.]*}"
 
 echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
 echo
-echo "$debVersion: ${url}@${commit}"
+#echo "$debVersion: ${url}@${commit}"
 echo "$fullVersion: ${url}@${commit}"
 echo "$version: ${url}@${commit}"
 echo "latest: ${url}@${commit}"
