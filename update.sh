76944da1e7d28af0fdfbfcd770b0663e4eb1a8fa
Tianon Gravi <admwiggin@gmail.com>
2015-05-14 16:03:08 -0600

    Update update.sh for 1.5 (#15)

--- update.sh
+++ update.sh
@@ -3,8 +3,8 @@ set -e
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
-version='1.4'
-fullVersion="$(curl -sSL "http://packages.elasticsearch.org/logstash/$version/debian/dists/stable/main/binary-amd64/Packages.gz" | gunzip | awk -F ': *' '$1 == "Package" { pkg = $2 } pkg == "logstash" && $1 == "Version" { print $2 }' | sort -V | tail -1)"
+version='1.5'
+fullVersion="$(curl -sSL "http://packages.elasticsearch.org/logstash/$version/debian/dists/stable/main/binary-amd64/Packages.gz" | gunzip | awk -F ': +' '$1 == "Package" { pkg = $2 } pkg == "logstash" && $1 == "Version" { print $2 }' | sort -V | tail -1)"
 
 sed -ri \
 	-e 's/^(ENV LOGSTASH_MAJOR) .*/\1 '"$version"'/' \
