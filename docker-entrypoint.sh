77ea0a99221dde6e8d19f9c78f2ce90fad68a702
Pasang Sherpa <pgsherpa12@gmail.com>
2015-05-25 01:21:49 -0700

    default config file provided -> https://github.com/docker-library/logstash/issues/16

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,12 +4,22 @@ set -e
 
 # Add logstash as command if needed
 if [[ "$1" == -* ]]; then
-	set -- logstash "$@"
+    set -- logstash "$@"
+else 
+    # Run as user "logstash" if the command is "logstash"
+    if [ "$1" == logstash ]; then
+        set -- gosu logstash "$@"
+    else
+        # As argument is not related to logstash, then
+        # assume that user wants to run their own process,
+        # for example a `bash` shell to explore this image
+        exec "$@"
+    fi
 fi
 
-# Run as user "logstash" if the command is "logstash"
-if [ "$1" == logstash ]; then
-	set -- gosu logstash "$@"
+# Use default config if CONFIGSTRING or CONFIGFILE is not provided
+if [[ "$*" == *-f* ]] || [[ "$*" == *--config* ]] || [[ "$*" == *-e* ]]; then
+    exec "$@"
+else 
+    exec "$@" -f /usr/share/logstash/config/logstash.conf
 fi
-
-exec "$@"
