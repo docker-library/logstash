060ef499f4f15e8591854e67caa78dd649cbc22d
Tianon Gravi <admwiggin@gmail.com>
2015-08-11 15:12:30 -0700

    Enhance "generate-stackbrew-library.sh" to only take into account changes to the Dockerfile or files from COPY in the Dockerfile for choosing the commit hash for a particular directory

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -3,7 +3,7 @@ set -e
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 url='git://github.com/docker-library/logstash'
-commit=$(git log -1 --format="format:%H" -- Dockerfile)
+commit="$(git log -1 --format='format:%H' -- Dockerfile $(awk 'toupper($1) == "COPY" { for (i = 2; i < NF; i++) { print $i } }' Dockerfile))"
 debVersion="$(grep -m1 'ENV LOGSTASH_VERSION' Dockerfile | cut -d' ' -f3)"
 fullVersion="${debVersion%%[-]*}" # strip "debian version"
 fullVersion="${fullVersion##*:}" # strip epoch
