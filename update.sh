a1fe8d1a3a430df5bc930cd1e2a70fcf55e76d01
Tianon Gravi <admwiggin@gmail.com>
2019-04-22 12:58:23 -0700

    Drop support for 5.x (now EOL upstream)
    This simplifies the `update.sh` script to focus only on the upstream-produced release artifacts.

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 versions=( "$@" )
@@ -8,93 +9,60 @@ if [ ${#versions[@]} -eq 0 ]; then
 fi
 versions=( "${versions[@]%/}" )
 
+tags="$(
+	git ls-remote --tags https://github.com/elastic/dockerfiles.git \
+		| cut -d/ -f3 \
+		| grep -E '^v' \
+		| cut -d^ -f1 \
+		| sort -uV
+)"
+
 travisEnv=
 for version in "${versions[@]}"; do
-	major="${version%%.*}"
-	debRepo="https://artifacts.elastic.co/packages/${major}.x/apt"
-	logstashPath='/usr/share/logstash/bin'
-	tarballUrlBase='https://artifacts.elastic.co/downloads'
-	if [ "$major" -lt 5 ]; then
-		debRepo="http://packages.elastic.co/logstash/$version/debian"
-		logstashPath='/opt/logstash/bin'
-		tarballUrlBase='https://download.elastic.co/logstash'
+	rcVersion="${version%-rc}"
+	rcGrepV='-v'
+	if [ "$version" != "$rcVersion" ]; then
+		rcGrepV=
 	fi
 
-	fullVersion="$(curl -fsSL "$debRepo/dists/stable/main/binary-amd64/Packages.gz" | gunzip | awk -F ': ' '$1 == "Package" { pkg = $2 } pkg == "logstash" && $1 == "Version" { print $2 }' | sort -rV | grep -F 1:$version | head -n1)"
+	fullVersion="$(
+		grep -P "^\Qv$rcVersion." <<<"$tags" \
+			| grep $rcGrepV -E -- '-(alpha|beta|rc)' \
+			| tail -1
+	)"
 	if [ -z "$fullVersion" ]; then
 		echo >&2 "warning: cannot find full version for $version"
 		continue
 	fi
-	# convert "1:5.0.2-1" over to "5.0.2"
-	plainVersion="${fullVersion%%-*}" # strip non-upstream-version
-	plainVersion="${plainVersion##*:}" # strip epoch
-	tilde='~'; plainVersion="${plainVersion//$tilde/-}" # replace '~' with '-'
+	fullVersion="${fullVersion#v}"
 
-	if [ $major -ge 6 ]; then
-		# Use the "upstream" Dockerfile, which rebundles the existing image from Elastic.
-		upstreamImage="docker.elastic.co/logstash/logstash:$plainVersion"
+	echo "$version: $fullVersion"
 
-		# Parse image manifest for sha
-		authToken="$(curl -fsSL 'https://docker-auth.elastic.co/auth?service=token-service&scope=repository:logstash/logstash:pull' | jq -r .token)"
-		digest="$(curl --head -fsSL -H 'Accept: application/vnd.docker.distribution.manifest.v2+json' -H "Authorization: Bearer $authToken" "https://docker.elastic.co/v2/logstash/logstash/manifests/$plainVersion" | tr -d '\r' | gawk -F ':[[:space:]]+' '$1 == "Docker-Content-Digest" { print $2 }')"
+	upstreamImage="docker.elastic.co/logstash/logstash:$fullVersion"
 
-		# Format image reference (image@sha)
-		upstreamImageDigest="$upstreamImage@$digest"
+	# Parse image manifest for sha
+	authToken="$(curl -fsSL 'https://docker-auth.elastic.co/auth?service=token-service&scope=repository:logstash/logstash:pull' | jq -r .token)"
+	digest="$(curl --head -fsSL -H 'Accept: application/vnd.docker.distribution.manifest.v2+json' -H "Authorization: Bearer $authToken" "https://docker.elastic.co/v2/logstash/logstash/manifests/$fullVersion" | tr -d '\r' | gawk -F ':[[:space:]]+' '$1 == "Docker-Content-Digest" { print $2 }')"
 
-		upstreamDockerfileLink="https://github.com/elastic/logstash-docker/tree/$plainVersion"
+	# Format image reference (image@sha)
+	upstreamImageDigest="$upstreamImage@$digest"
 
-		(
-			set -x
-			curl -fsSL -o /dev/null "$upstreamDockerfileLink" # make sure the upstream Dockerfile link exists
-			sed '
-				s!%%LOGSTASH_VERSION%%!'"$plainVersion"'!g;
-				s!%%UPSTREAM_IMAGE_DIGEST%%!'"$upstreamImageDigest"'!g;
-				s!%%UPSTREAM_DOCKERFILE_LINK%%!'"$upstreamDockerfileLink"'!g;
-			' Dockerfile-upstream.template > "$version/Dockerfile"
-		)
-		travisEnv='\n  - VERSION='"$version VARIANT=$travisEnv"
-	else
-		(
-			set -x
-			cp docker-entrypoint.sh "$version/"
-			sed '
-				s!%%LOGSTASH_VERSION%%!'"$plainVersion"'!g;
-				s!%%LOGSTASH_DEB_REPO%%!'"$debRepo"'!g;
-				s!%%LOGSTASH_DEB_VERSION%%!'"$fullVersion"'!g;
-				s!%%LOGSTASH_PATH%%!'"$logstashPath"'!g;
-			' Dockerfile-debian.template > "$version/Dockerfile"
-		)
-	
-		if [ -d "$version/alpine" ]; then
-			tarball="$tarballUrlBase/logstash/logstash-${plainVersion}.tar.gz"
-			tarballAsc="${tarball}.asc"
-			if ! wget --quiet --spider "$tarballAsc"; then
-				tarballAsc=
-			fi
-			tarballSha1=
-			for sha1Url in "${tarball}.sha1" "${tarball}.sha1.txt"; do
-				if sha1="$(wget -qO- "$sha1Url")"; then
-					tarballSha1="${sha1%% *}"
-					break
-				fi
-			done
-			(
-				set -x
-				cp docker-entrypoint.sh "$version/alpine/"
-				sed -i 's/gosu/su-exec/g' "$version/alpine/docker-entrypoint.sh"
-				sed \
-					-e 's!%%LOGSTASH_VERSION%%!'"$plainVersion"'!g' \
-					-e 's!%%LOGSTASH_PATH%%!'"$logstashPath"'!g' \
-					-e 's!%%LOGSTASH_TARBALL%%!'"$tarball"'!g' \
-					-e 's!%%LOGSTASH_TARBALL_ASC%%!'"$tarballAsc"'!g' \
-					-e 's!%%LOGSTASH_TARBALL_SHA1%%!'"$tarballSha1"'!g' \
-					Dockerfile-alpine.template > "$version/alpine/Dockerfile"
-			)
-			travisEnv='\n  - VERSION='"$version VARIANT=alpine$travisEnv"
-		fi
-		travisEnv='\n  - VERSION='"$version VARIANT=$travisEnv"
-	fi
+	upstreamDockerfileLink="https://github.com/elastic/logstash-docker/tree/$fullVersion"
+	upstreamDockerfile="${upstreamDockerfileLink//tree/raw}/Dockerfile"
+
+	(
+		set -x
+		curl -fsSL -o /dev/null "$upstreamDockerfileLink" # make sure the upstream Dockerfile link exists
+		# TODO curl -fsSL "$upstreamDockerfile" | grep -P "\Q$fullVersion" # ... and that it contains the right version
+	)
+
+	sed -e 's!%%LOGSTASH_VERSION%%!'"$fullVersion"'!g' \
+		-e 's!%%UPSTREAM_IMAGE_DIGEST%%!'"$upstreamImageDigest"'!g' \
+		-e 's!%%UPSTREAM_DOCKERFILE_LINK%%!'"$upstreamDockerfileLink"'!g' \
+		Dockerfile.template > "$version/Dockerfile"
+
+	travisEnv='\n  - VERSION='"$version$travisEnv"
 done
 
 travis="$(awk -v 'RS=\n\n' '$1 == "env:" { $0 = "env:'"$travisEnv"'" } { printf "%s%s", $0, RS }' .travis.yml)"
-echo "$travis" > .travis.yml
+cat <<<"$travis" > .travis.yml
